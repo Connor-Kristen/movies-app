29e3f6eeecd104527b43339e03d4d6571d170486
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-03 12:03:26 -0600

    added styling and basic read functionality

---
+++ build/js/crud.js
@@ -0,0 +1,18 @@
+const moviesURL = 'https://mewing-typical-stage.glitch.me/movies';
+const omdbMainURL =  `http://www.omdbapi.com/?apikey=${omdbKey}`
+const header = {'Content-Type': 'application/json'};
+// i=tt3896198&
+
+const getMovies = () => {
+    return fetch(moviesURL)
+        .then(response => response.json())
+        .catch(console.error)
+}
+
+getMovies().then(console.log)
+
+const getMovieId = (id) => {
+    return fetch(`${moviesURL}/${id}`)
+        .then(response => response.json())
+        .catch(console.error)
+}
